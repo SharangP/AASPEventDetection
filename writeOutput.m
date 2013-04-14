cc8a9d4108c933620c19ea8dabe263df44f6233c
csherland <c.sherland@yahoo.com>
2013-04-14 10:46:12 -0400

    Preparing for contest submission.

---
+++ writeOutput.m
@@ -0,0 +1,14 @@
+function writeOutput( outputFile )
+%WRITEOUTPUT writes analysis of event data 
+%   to the file name specified by outputFile
+
+fid = fopen(outputFile,'w');
+
+if fid ~= -1
+   
+    
+    
+end
+
+end
+
